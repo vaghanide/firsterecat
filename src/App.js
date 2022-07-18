868319b651e7ac6736746e1caa3c8047a1289a7b
vaghanide <vaghanideep31@gmail.com>
2022-07-18 09:03:52 +0530

    13-7-22reduce

--- src/App.js
+++ src/App.js
@@ -2,88 +2,136 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let data = 
+  // let data = 
+
+  // [
+  //   {
+  //     id: 101,
+  //     name: 'Abacavir',
+  //     quantity: 25,
+  //     price: 150,
+  //     expiry: 2022,
+  //     status: true
+  //   },
+  //   {
+  //     id: 102,
+  //     name: 'Eltrombopag',
+  //     quantity: 90,
+  //     price: 550,
+  //     expiry: 2021,
+  //     status: true
+  //   },
+  //   {
+  //     id: 103,
+  //     name: 'Meloxicam',
+  //     quantity: 85,
+  //     price: 450,
+  //     expiry: 2025,
+  //     status: false
+  //   },
+  //   {
+  //     id: 104,
+  //     name: 'Allopurinol',
+  //     quantity: 50,
+  //     price: 600,
+  //     expiry: 2023,
+  //     status: true
+  //   },
+  //   {
+  //     id: 105,
+  //     name: 'Phenytoin',
+  //     quantity: 63,
+  //     price: 250,
+  //     expiry: 2021,
+  //     status: false
+  //   }
+  // ];
+
+  // return (
+  //   <div>
+  //     {Data.map((v, i) => {
+  //       return (
+  //         <div key={i}>
+  //           <h1>{v.name}</h1>
+  //           <h2>{v.price}</h2>
+  //         </div>
+  //       );
+  //     })}
+  //   </div>
+  // );
+
+  // //filter
+
+  // let fData = Data.filter((v, i) => v.expiry >= 2022 && v.status === true);
+  // console.log(fData);
+
+  // //reduce
+
+  // let totalprice = fData.reduce((acc, v, i) => acc + v.price, 0 );
+  // console.log(totalprice)
 
 
-  [
-    {
-      name: "amit",
-      age: 35,
-      salary: 40000,
-      bonus: 1000,
-      status: true
-    },
-    {
-      name: "ajay",
-      age: 25,
-      salary: 38000,
-      bonus: 2000,
-      status: false
-    },
-    {
-      name: "mayur",
-      age: 23,
-      salary: 50000,
-      bonus: 500,
-      status: true
-    },
-    {
-      name: "jay",
-      age: 29,
-      salary: 35000,
-      bonus: 600,
-      status: true
-    },
-    {
-      name: "raj",
-      age: 33,
-      salary: 22000,
-      bonus: 2000,
-      status: true
-    },
-  ]
-    return(
-      <div>
-        {
-          data.map((v,i)=>{
-            return(
-              <div> 2
-                <h1>{v.name}</h1>
-                <h2>{v.salary}</h2>
-              </div>
-            )
-          }
-          )
-        }
-      </div>
-    )
 
-    let fdata= data.filter((v,i) =>v.status === true);
-    console.log(data);
+//   //  Employee Data
 
-    let totalprice=fdata.reduse((acc,v,i)=>acc+v.price , 0);
-    console.log(totalprice);
+//   let data= 
+//   [
+//     {
+//       name: "amit",
+//       age: 35,
+//       salary: 40000,
+//       bonus: 1000,
+//       status: true
+//     },
+//     {
+//       name: "ajay",
+//       age: 25,
+//       salary: 38000,
+//       bonus: 2000,
+//       status: false
+//     },
+//     {
+//       name: "mayur",
+//       age: 23,
+//       salary: 50000,
+//       bonus: 500,
+//       status: true
+//     },
+//     {
+//       name: "jay",
+//       age: 29,
+//       salary: 35000,
+//       bonus: 600,
+//       status: true
+//     },
+//     {
+//       name: "raj",
+//       age: 33,
+//       salary: 22000,
+//       bonus: 2000,
+//       status: true
+//     },
+//   ];
+// //filter
+// let fdata = data.filter((v, i) => v.salary >= 2022 && v.status === true);
+// console.log(fdata);
 
- 
-  return (
-    
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-  }
+// //reduce
+// let totalprice = fdata.reduce((acc, v, i) => acc + v.price, 0 );
+// console.log(totalprice);
 
-export default App;
+//   return (
+//     <div>
+//       {data.map((v, i,) => {
+//         return (
+//           <div key={i}>
+//             <h1>{v.name}</h1>
+//             <h2>{v.salary}</h2>
+//             <h3>{v.bonus}</h3>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// };
+// };
